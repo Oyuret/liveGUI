f64b1c7c041056c17fee572cdefb66705709e399
Yuri Stange <yuris@kth.se>
2014-07-07 00:38:16 +0200

    Small changes to the games delegate

--- gamesitemdelegate.cpp
+++ gamesitemdelegate.cpp
@@ -19,27 +19,59 @@ void GamesItemDelegate::paint(QPainter *painter,
                               const QStyleOptionViewItem &option,
                               const QModelIndex &index) const
 {
-    QString game_name = index.model()->data(index, ROLE_NAME).toString();
+    painter->save();
 
-    QStyleOptionViewItem myOption = option;
-    myOption.displayAlignment = Qt::AlignRight | Qt::AlignVCenter;
+    QFont font = QApplication::font();
+    QFont SubFont = QApplication::font();
+    font.setPixelSize(20);
+    font.setBold(true);
+    SubFont.setWeight(SubFont.weight()-2);
+    QFontMetrics fm(font);
 
-    drawDisplay(painter, myOption, myOption.rect, game_name);
-    drawFocus(painter, myOption, myOption.rect);
+    QIcon icon(":/icons/icons/favorite.png");
+    QString headerText = qvariant_cast<QString>(index.data(ROLE_NAME));
+    QString subText = qvariant_cast<QString>(index.data(ROLE_VIEWERS));
 
-    QString text2 = index.model()->data(index, ROLE_VIEWERS).toString();
+    QSize iconsize = QSize(20,20);
 
-    QStyleOptionViewItem myOption2 = option;
-    myOption.displayAlignment = Qt::AlignLeft | Qt::AlignVCenter;
+    QRect headerRect = option.rect;
+    QRect subheaderRect = option.rect;
+    QRect iconRect = subheaderRect;
 
-    drawDisplay(painter, myOption2, myOption2.rect, text2);
-    drawFocus(painter, myOption2, myOption2.rect);
+    iconRect.setRight(iconsize.width()+30);
+    iconRect.setTop(iconRect.top()+5);
+    headerRect.setLeft(iconRect.right());
+    subheaderRect.setLeft(iconRect.right());
+    headerRect.setTop(headerRect.top()+5);
+    headerRect.setBottom(headerRect.top()+fm.height());
+
+    subheaderRect.setTop(headerRect.bottom()+2);
+
+
+    //painter->drawPixmap(QPoint(iconRect.right()/2,iconRect.top()/2),icon.pixmap(iconsize.width(),iconsize.height()));
+    painter->drawPixmap(QPoint(iconRect.left()+iconsize.width()/2+2,iconRect.top()+iconsize.height()/2+3),icon.pixmap(iconsize.width(),iconsize.height()));
+
+    painter->setFont(font);
+    painter->drawText(headerRect,headerText);
+
+
+    painter->setFont(SubFont);
+    painter->drawText(subheaderRect.left(),subheaderRect.top()+17,subText);
+
+    drawFocus(painter, option, option.rect);
+
+    painter->restore();
 }
 
-QSize GamesItemDelegate::sizeHint(const QStyleOptionViewItem &/*option*/,
-                       const QModelIndex &/*index*/) const
+QSize GamesItemDelegate::sizeHint(const QStyleOptionViewItem &  option ,
+                                  const QModelIndex & index) const
 {
-    //hard coding size for test purpose,
-    //actual size hint can be calculated from option param
-    return QSize(100,50);
+    //QIcon icon = qvariant_cast<QIcon>(index.data(IconRole));
+    QSize iconsize = QSize(20,20);
+    QFont header_font = QApplication::font();
+    header_font.setPixelSize(20);
+    QFontMetrics fm(header_font);
+
+    return(QSize(iconsize.width(),iconsize.height()+fm.height() + 12 ));
+
 }
